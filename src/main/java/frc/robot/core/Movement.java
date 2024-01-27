20590eb89f14a957b0a8898bbbc7a1e68c73ae66
BuildTools <unconfigured@null.spigotmc.org>
2024-01-26 17:23:49 -0800

    Check Closest rotation direction implemented

--- src/main/java/frc/robot/core/Movement.java
+++ src/main/java/frc/robot/core/Movement.java
@@ -3,4 +3,21 @@ package frc.robot.core;
 
 public class Movement {
 
+
+
+
+
+
+    public boolean isClockwiseCloser (double cRotation, double tRotation) {
+        boolean output;
+        double nRotation = cRotation-tRotation;
+        if (nRotation>=180){
+            output = true;
+        }
+        else {
+            output = false;
+        };
+
+        return output;
+    };
 }
