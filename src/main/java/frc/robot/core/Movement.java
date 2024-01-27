b7a97d225682cbf6c9667819415de468d1ea6145
BuildTools <unconfigured@null.spigotmc.org>
2024-01-26 18:27:30 -0800

    Movement Angle Calculator Implemented

--- src/main/java/frc/robot/core/Movement.java
+++ src/main/java/frc/robot/core/Movement.java
@@ -1,6 +1,14 @@
-
 package frc.robot.core;
+import edu.wpi.first.wpilibj.XboxController;
 
 public class Movement {
-
+    public static double Calculate_angle (XboxController x) {
+        double[] a = input.getAxis(x);
+        double leftX = a[0];
+        double leftY = a[1];
+        double tangent = leftY/leftX;
+        double radians = Math.atan(tangent);
+        double angle = Math.toDegrees(radians);
+        return angle;
+    };
 }
